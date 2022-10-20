fa7fc1996ef2396757a5442438d79b722f042002
Alessandro <professornpc@gmail.com>
2022-10-20 19:53:19 -0300

    Projeto Base

---
+++ src/main/java/com/belval/transporteescolar/TransporteescolarApplication.java
@@ -0,0 +1,13 @@
+package com.belval.transporteescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteescolarApplication.class, args);
+	}
+
+}
